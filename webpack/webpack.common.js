02fa008fe04f779bd9f8ba9ca0c0e2ce0f577a87
Jo <loluvulol@gmail.com>
2022-10-05 13:36:07 +1000

    Update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -54,7 +54,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: "./docs/index.html",
+			template: "public/index.html",
 			favicon: "public/favicon.ico",
 		}),
 		new webpack.optimize.SplitChunksPlugin({
